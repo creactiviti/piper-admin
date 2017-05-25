c43acee13db314e5a82f9c8d82ea5f407b479a67
creactiviti <arik.c.mail@gmail.com>
2017-05-25 15:01:43 -0700

    wip

--- src/main/java/com/creactiviti/piper/admin/web/JobsController.java
+++ src/main/java/com/creactiviti/piper/admin/web/JobsController.java
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
-public class HomeController {
+public class JobsController {
 
   private final RestTemplate rest = new RestTemplate();
   
-  @GetMapping("/")
+  @GetMapping("/jobs")
   public String home (@RequestParam(value="p",defaultValue="1") Integer aPage, Model aModel) {
     Map<String,Object> jobs = rest.getForObject("http://localhost:8080/jobs?p="+aPage, Map.class);
     aModel.addAttribute("jobs",jobs);
     aModel.addAttribute("page",aPage);
-    return "home";
+    return "jobs";
   }
   
 }
