b04af987a3e82ddddcb7d2f807aba43eb8d6155f
creactiviti <arik.c.mail@gmail.com>
2017-05-25 15:28:55 -0700

    wip

--- src/main/java/com/creactiviti/piper/admin/web/JobsController.java
+++ src/main/java/com/creactiviti/piper/admin/web/JobsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,11 +19,18 @@ public class JobsController {
   private String api = "http://localhost:8080"; 
   
   @GetMapping("/jobs")
-  public String home (@RequestParam(value="p",defaultValue="1") Integer aPage, Model aModel) {
+  public String list (@RequestParam(value="p",defaultValue="1") Integer aPage, Model aModel) {
     Map<String,Object> jobs = rest.getForObject(String.format("%s/jobs?p=%s",api,aPage), Map.class);
     aModel.addAttribute("jobs",jobs);
     aModel.addAttribute("page",aPage);
     return "jobs";
+  }
+  
+  @GetMapping("/jobs/{id}")
+  public String get (@PathVariable("id") String aJobId, Model aModel) {
+    Map<String,Object> jobs = rest.getForObject(String.format("%s/jobs/%s",api,aJobId), Map.class);
+    aModel.addAttribute("job",jobs);
+    return "job";
   }
   
 }
